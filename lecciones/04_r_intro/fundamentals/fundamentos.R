8f80ea5f18022d5762cfcfe669450d6d216a8df4
macchid <macchi.dm@gmail.com>
2017-11-14 11:57:58 -0300

    - Se agrega archivos rhistory al .gitignore. - Se guardan las ultimas modificaciones al codigo fuente en R.

--- lecciones/04_r_intro/fundamentals/fundamentos.R
+++ lecciones/04_r_intro/fundamentals/fundamentos.R
@@ -1,1 +1,27 @@
-v <- c()
+v <- c(0.5,0.6)
+l1 <- c(FALSE, TRUE, TRUE)
+l2 <- c(T, F)
+ch <- c('a')
+it <- 9:29
+co <- c(1+0i, 2+4i)
+v2 <- vector('numeric', 10)
+v2[1] <- 5
+
+
+x <- 1:3
+y <- 10:12
+
+z <- cbind(x,y)
+z
+
+w <- rbind(x,y)
+w
+
+x <- factor(c('yes', 'yes', 'no', 'yes', 'no'))
+x
+
+table(x)
+
+
+df <- data.frame(c1 = 1:4, c2 = c(T, T, F, F, T), c3 = c('a', 'b', 'c', 'd', 'e'))
+df
